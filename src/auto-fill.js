e66299fb728bbef68c84c945b62f1e9da80e0841
FlowGurus <101252355+FlowGurus@users.noreply.github.com>
2022-05-20 22:33:30 +0300

    feat: add auto-fill script

---
+++ src/auto-fill.js
@@ -0,0 +1,97 @@
+class WebflowMagic_AutoFill {
+  localOptions = {}
+  defaultOptions = {
+    HIDE_QUERY_PARAMS: false,
+    ATTRIBUTE_PREFIX: 'magic-autofill-',
+    QUERY_PARAM_PREFIX: 'magic_',
+  }
+
+  constructor(options) {
+    if (options && typeof options === 'object') {
+      Object.entries(this.defaultOptions).forEach(([option, value]) => {
+        this.localOptions[option] = options[option] == null ? value : options[option];
+      });
+    } else {
+      this.localOptions = { ...this.defaultOptions };
+    }
+
+    this.mount();
+  }
+
+  mount() {
+
+    document.querySelectorAll(`[${this.localOptions.ATTRIBUTE_PREFIX}element=item`).forEach((itemNode) => {
+      const linkNodes = itemNode.querySelectorAll(`[${this.localOptions.ATTRIBUTE_PREFIX}element=link]`);
+      if (linkNodes.length === 0) {
+        console.warn(`No elements with attribute "${this.localOptions.ATTRIBUTE_PREFIX}element"="link" was found inside item`, itemNode);
+        return;
+      }
+
+      if (this.localOptions.HIDE_QUERY_PARAMS) {
+        linkNodes.forEach(el => {
+          el.addEventListener('click', (event) => {
+            const params = this.getItemParams(itemNode);
+            localStorage.setItem(`${this.localOptions.ATTRIBUTE_PREFIX}data`, JSON.stringify(params));
+          });
+        });
+      } else {
+        const params = this.getItemParams(itemNode, this.localOptions.QUERY_PARAM_PREFIX);
+        
+        linkNodes.forEach(el => {
+          const newUrl = new URL(el.href);
+          for(let [key, value] of Object.entries(params)) {
+            newUrl.searchParams.set(key,value);
+          }
+          el.href = newUrl.toString();
+        });
+      }
+    });
+
+    document.querySelectorAll(`[${this.localOptions.ATTRIBUTE_PREFIX}element=form]`).forEach((formNode) => {
+      if (this.localOptions.HIDE_QUERY_PARAMS) {
+        const params = JSON.parse(localStorage.getItem(`${this.localOptions.ATTRIBUTE_PREFIX}data`));
+        if (params && typeof params === 'object') {
+          for(let [fieldName, value] of Object.entries(params)) {
+            this.setFormFieldValue(formNode, fieldName, value); 
+          }
+        }
+      } else {
+        const url = new URL(window.location);
+        url.searchParams.forEach((value, key) => {
+          if (key.startsWith(this.localOptions.QUERY_PARAM_PREFIX)) {
+            const fieldName = key.replace(this.localOptions.QUERY_PARAM_PREFIX, '');
+            this.setFormFieldValue(formNode, fieldName, value);
+          }
+        });
+      }
+    });
+  }
+  
+  getItemParams(itemNode, namePrefix='') {
+    const params = {};
+    const paramNodes = itemNode.querySelectorAll(`[${this.localOptions.ATTRIBUTE_PREFIX}text]`);
+    if (paramNodes.length === 0) {
+      console.warn(`No elements with attribute "${this.localOptions.ATTRIBUTE_PREFIX}text" was found inside item`, itemNode);
+      return;
+    }
+    paramNodes.forEach((el) => {
+      const value = el?.textContent?.trim();
+      const name = el.attributes[`${this.localOptions.ATTRIBUTE_PREFIX}text`]?.value?.trim();
+      if (name) params[namePrefix+name] = value;
+    });
+    return params;
+  }
+
+  setFormFieldValue(formNode, fieldName, value) {
+    // for now only textareas and input type=text|hidden is supported
+    let inputNode = formNode.querySelector(`textarea[name=${fieldName}],input[type=text][name=${fieldName}],input[type=hidden][name=${fieldName}]`);
+    if (inputNode == null) {
+      inputNode = document.createElement('input');
+      inputNode.type = 'hidden';
+      inputNode.name = fieldName;
+      formNode.append(inputNode);
+    }
+    inputNode.value = value;
+  }
+
+}
