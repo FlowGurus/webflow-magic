59c33ff4c768bc19d3a13d8af2494830e6838b81
FlowGurus <101252355+FlowGurus@users.noreply.github.com>
2022-05-26 10:34:23 +0300

    add viewport animation pre alpha

---
+++ src/viewport-animation.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) entry.target.classList.add('is-in-viewport');
+    else entry.target.classList.remove('is-in-viewport');
+  });
+});
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('[class^="text-magic-"]').forEach(el => observer.observe(el));
+  document.querySelectorAll('[class^="ani-magic-"]').forEach(el => observer.observe(el));
+});
