9d284c4d6e3d26d6d587ff40cf03f90ad25f7288
FlowGurus <101252355+FlowGurus@users.noreply.github.com>
2022-06-19 16:35:20 +0300

    fix(viewport-animation): wrap logic

--- src/viewport-animation.js
+++ src/viewport-animation.js
@@ -1,11 +1,13 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    console.log(entry);
-    if (entry.isIntersecting) entry.target.classList.add('is-in-viewport');
-    else entry.target.classList.remove('is-in-viewport');
+(() => {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) entry.target.classList.add('is-in-viewport');
+      else entry.target.classList.remove('is-in-viewport');
+    });
   });
-});
-window.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('[class^="text-magic-"]').forEach(el => observer.observe(el));
-  document.querySelectorAll('[class^="ani-magic-"]').forEach(el => observer.observe(el));
-});
+  window.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('[class^="text-magic-"]').forEach(el => observer.observe(el));
+    document.querySelectorAll('[class^="ani-magic-"]').forEach(el => observer.observe(el));
+  });
+})();
+  
