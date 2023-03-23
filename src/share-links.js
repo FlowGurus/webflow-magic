c3bf7d8d42baa75d5d26f85ac4c8cfc3d33da552
gaabora <gaabora@gmail.com>
2023-03-24 00:38:27 +0200

    feat(share-links): attribute wip

--- src/share-links.js
+++ src/share-links.js
@@ -1,17 +1,22 @@
 class WebflowMagic_ShareLinks {
   localOptions = {}
   defaultOptions = {
-    LINK_CLASS: 'share-link',
-    LINK_CLASS_CLIPBOARD: 'clipboard',
-    LINK_CLASS_FACEBOOK: 'facebook',
-    LINK_CLASS_LINKEDIN: 'linkedin',
-    LINK_CLASS_TWITTER: 'twitter',
+    ATTRIBUTE_PREFIX: 'magic-sharelinks-',
     ADD_UTM: false,
     SET_TARGET_BLANK: true,
     SET_UTM_SOURCE: true,
     UTM_STATIC_DATA: {
       utm_medium: 'share-link',
     },
+    POPUP_TIMEOUT: 3000,
+    POPUP_ERROR_TEXT: "Unable to copy the link to clipboard",
+    POPUP_PROMPT_TEXT: "You can copy the link over here",
+    //obsolete options
+    LINK_CLASS: 'share-link',
+    LINK_CLASS_CLIPBOARD: 'clipboard',
+    LINK_CLASS_FACEBOOK: 'facebook',
+    LINK_CLASS_LINKEDIN: 'linkedin',
+    LINK_CLASS_TWITTER: 'twitter',
   }
 
 
@@ -38,6 +43,25 @@ class WebflowMagic_ShareLinks {
     }
 
     const url = new URL(`${location.origin}${location.pathname}`);
+
+    
+    document.querySelectorAll(`[${this.localOptions.ATTRIBUTE_PREFIX}target]`).forEach(linkEl => {
+      const destinationType = linkEl.getAttribute(`${this.localOptions.ATTRIBUTE_PREFIX}target`);
+      
+      if (this.localOptions.ADD_UTM && this.localOptions.SET_UTM_SOURCE) searchParams.set('utm_source', destinationType);
+      
+      url.search = searchParams.toString();
+
+      if (destinationType === 'clipboard') linkEl.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.copyTextToClipboard(url.toString());
+      });
+      
+      this.setlinkUrl(linkEl, this.getShareLink(destinationType, url.toString()));
+      if (this.localOptions.SET_TARGET_BLANK) linkEl.target = '_blank';
+    });
+    
+    // support of obsolete logic
     document.querySelectorAll(`.${this.localOptions.LINK_CLASS}`).forEach(linkEl => {
       const destinationType = this.getDestinationType(linkEl);
       
@@ -74,6 +98,7 @@ class WebflowMagic_ShareLinks {
     }
   }
   fallbackCopyTextToClipboard(text) {
+    let isSuccess = true;
     var textArea = document.createElement("textarea");
     textArea.value = text;
     textArea.style.top = "0";
@@ -88,14 +113,40 @@ class WebflowMagic_ShareLinks {
       document.execCommand('copy');
     } catch (ex) {
       console.error('Fallback: Oops, unable to copy', ex);
+      isSuccess = false
     }
     document.body.removeChild(textArea);
+    return isSuccess;
   }
   copyTextToClipboard(text) {
     if (!navigator.clipboard) {
-      this.fallbackCopyTextToClipboard(text);
+      this.showPopup(this.fallbackCopyTextToClipboard(text) ? null : this.localOptions.POPUP_ERROR_TEXT);
     } else {
-      navigator.clipboard.writeText(text);
+      navigator.clipboard.writeText(text).then(() => {
+        this.showPopup();
+      }, () => {
+        try {
+          window.prompt(this.localOptions.POPUP_PROMPT_TEXT, text);
+        } catch (ex) {
+          console.error('Fallback: Oops, unable to copy', ex);
+          this.showPopup(this.localOptions.POPUP_ERROR_TEXT);
+        }
+      });
+    }
+  }
+  showPopup(error = null) {
+    const selector = (error) 
+      ? `[${this.localOptions.ATTRIBUTE_PREFIX}popup=fail]`
+      : `[${this.localOptions.ATTRIBUTE_PREFIX}popup=success]`
+    ;
+    const popupEl = document.querySelector(selector);
+    if (popupEl) {
+      popupEl.style.display = 'block';
+      setTimeout(() => {
+        popupEl.style.display = 'none';
+      }, this.localOptions.POPUP_TIMEOUT);
+    } else {
+      console.warn(`No popup elements found by selector ${selector}`);
     }
   }
 }
